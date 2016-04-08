cd62914a4c7e275ee195bcc4988a0b67e52978e6
PotoYang <715792648@qq.com>
2016-04-08 21:10:33 +0800

    2016.4.8 yyc修改启动画面

--- app/src/main/java/com/example/potoyang/bikelock/LoadingActivity.java
+++ app/src/main/java/com/example/potoyang/bikelock/LoadingActivity.java
@@ -2,36 +2,44 @@ package com.example.potoyang.bikelock;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
-import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+/**
+ * @ClassName: com.example.potoyang.bikelock.LoadingActivity
+ * @Author: PotoYang
+ * @Time: 2016/4/8 21:00
+ * @Description: app启动画面
+ */
 public class LoadingActivity extends AppCompatActivity {
+
+    private LinearLayout ll;
+    private TextView tv_version;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //AndroidStudio隐藏标题栏由于是继承至AppCompatActivity不一样
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_loading);
         super.onCreate(savedInstanceState);
 
-        /*
-         *AndroidStudio隐藏标题栏由于是继承至AppCompatActivity不一样
-         */
-        getSupportActionBar().hide();
-        final View view = View.inflate(this, R.layout.activity_loading, null);
+        ll = (LinearLayout) findViewById(R.id.splash_root);
+        tv_version = (TextView) findViewById(R.id.tv_version);
 
-        /*
-         *设置亮度渐变,从0.0到1.0
-         */
+        tv_version.setText(getVersion());
+        //设置亮度渐变,从0.0到1.0
         Animation animation = AnimationUtils.loadAnimation(this, R.anim.alpha);
-        view.startAnimation(animation);
+        ll.startAnimation(animation);
 
-        setContentView(view);
-
-        /*
-         *设置动画监听器，当动画结束的时候，启动新的Activity
-         */
+        //设置动画监听器，当动画结束的时候，启动新的Activity
         animation.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
@@ -40,10 +48,9 @@ public class LoadingActivity extends AppCompatActivity {
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                /*
-                 * SharedPreferences存储以键值对的方式存储
-                 * getSharedPreferences的第一个参数就是文件名称
-                 */
+
+                //SharedPreferences存储以键值对的方式存储
+                //getSharedPreferences的第一个参数就是文件名称
                 SharedPreferences sharedPreferences = getSharedPreferences("share", MODE_PRIVATE);
                 boolean isFirstRun = sharedPreferences.getBoolean("isFirstRun", true);
                 SharedPreferences.Editor editor = sharedPreferences.edit();
@@ -63,20 +70,50 @@ public class LoadingActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * @MethodName: getVersion
+     * @Author: PotoYang
+     * @Time: 2016/4/8 20:59
+     * @Description: 获得app版本信息
+     */
+    private String getVersion() {
+        PackageManager pm = getPackageManager();
+        try {
+            PackageInfo packinfo = pm.getPackageInfo(getPackageName(), 0);
+            String version = packinfo.versionName;
+            return "Version:" + version;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return "版本号错误";
+        }
+    }
+
+    /**
+     * @MethodName: startPotocolActivity
+     * @Author: PotoYang
+     * @Time: 2016/4/8 21:01
+     * @Description: 跳转到ProtocolActivity
+     */
     private void startPotocolActivity() {
         Intent intent = new Intent(this, ProtocolActivity.class);
         startActivity(intent);
         finish();
     }
 
+    /**
+     * @MethodName: startMapActivity
+     * @Author: PotoYang
+     * @Time: 2016/4/8 21:01
+     * @Description: 跳转到startMapActivity
+     */
     private void startMapActivity() {
         Intent intent = new Intent(this, MapActivity.class);
         startActivity(intent);
         finish();
     }
 
-    /*
-     *屏蔽返回键
+    /**
+     * 屏蔽返回键
      */
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
